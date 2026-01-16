d6f7539044a48079632687d5e8b55f471795e614
Rabindra Dhami <50691743+rabindradhami@users.noreply.github.com>
2026-01-16 14:32:10 +0545

    fix(): jenkinsfile updated

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,9 +1,11 @@
 @Library("jenkins-library") _
 
 dockerImagePipeline(
-    script: this,
-    service: "test",
-    k8sDeploy: false,
-    slackChannel: "#infrastructure",
-    app: "test"
+  script: this,
+  service: "test",
+  dockerfile: "Dockerfile",
+  buildContext: ".",
+  pushToRegistry: false,
+  k8sDeploy: false,
+  slackChannel: "#test-devops"
 )
